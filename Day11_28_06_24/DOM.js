9d0a2a8dccdc39eaf0b24ebbc863a623b4c679f1
vasukilli <124597600+vasukilli@users.noreply.github.com>
2024-07-01 23:27:26 +0530

    Add files via upload

---
+++ Day11_28_06_24/DOM.js
@@ -0,0 +1,10 @@
+// alert("Hello world")
+// console.log("hi");
+// debugger;
+let heading1 = document.getElementById("heading");
+console.dir(heading1);
+let h1 = document.querySelector("h1");
+//console.dir(h2.innerText);
+let str = "from Apna College Students";
+h1.innerText = h1.innerText + str;
+//console.log("Hello vasudeva rao");
